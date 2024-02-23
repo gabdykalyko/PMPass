d1132c3b4b7201a79a30fa0782f359368adbcdfc
oral.gabdykalyk <oral.gabdykalyk@pmbet.kz>
2024-02-23 19:09:26 +0500

    feat: edited deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-cd
 cd Desktop/MyProjects/pmpass/
 git checkout master
 git checkout -B deployment
 git rm -r .
-rsync -av --exclude=node_modules/ build/ .
+cp -r build/* . --exclude=node_modules
 git add .
 git commit -m "Deploying latest build"
 git push origin deployment
