31ae0dd2ded9502f8ca7dc745e95b338ece45eff
oral.gabdykalyk <oral.gabdykalyk@pmbet.kz>
2024-02-23 20:06:48 +0500

    feat: edit deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-cd Desktop/MyProjects/pmpass/
-git checkout master
-git checkout -B deployment
-git pull
-git rm -r .
-cp -r build/. .
-rm -rf node_modules
+git checkout deployment
+git reset --hard origin/master
+npm install
+npm run build
+find * -maxdepth 0 -name 'build' -prune -o -exec rm -rf '{}' ';'
+mv ./build/* .
+git rm -rf --cache .
 git add .
-git commit -m "Deploying latest build"
-git push origin deployment
+git commit -m "deploy"
+git push origin deployment --force
 git checkout master
