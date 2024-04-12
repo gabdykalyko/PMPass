c7d6bd74edda6fcbd8b8da5937c0d0686ee5f1b4
oral.gabdykalyk <oral.gabdykalyk@pmbet.kz>
2024-04-12 13:18:18 +0500

    feat: added edit trade link

--- src/components/TradeLink/TradeLink.jsx
+++ src/components/TradeLink/TradeLink.jsx
@@ -10,6 +10,7 @@ import { updateAuth } from '../../slices/authSlice'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Toast from '../../components/Toast/Toast'
+import edit from '../../assets/images/icons/edit.svg'
 
 const TradeLink = ({ handleTradeClick }) => {
   const user = useSelector(state => state.auth.user)
@@ -18,6 +19,8 @@ const TradeLink = ({ handleTradeClick }) => {
 
   const [link, setLink] = useState('')
   const [isValid, setIsValid] = useState(true)
+
+  const [editTrade, setEditTrade] = useState(false)
 
   const handleChange = (e) => {
     const value = e.target.value
@@ -48,43 +51,111 @@ const TradeLink = ({ handleTradeClick }) => {
         toast(<Toast message="Ссылка на обмен прикреплена" />, {
           hideProgressBar: true
         });
+
+        setEditTrade(false)
       }
 
     } catch (error) {
+      setIsValid(false)
+      toast(<Toast message="Неверный формат ссылки на обмен"
+        status='error' />, {
+        hideProgressBar: true
+      });
       console.error(error);
     }
   }
 
+  const handleEditTrade = () => {
+    setEditTrade(true)
+  }
+
+  const cancelEdit = () => {
+    setEditTrade(false)
+    setIsValid(true)
+    setLink(user.steam_trade_url)
+  }
+
   const Enter = () => {
     if (!isValid) {
+      toast(<Toast message="Неверный формат ссылки на обмен"
+        status='error' />, {
+        hideProgressBar: true
+      });
+
       return
     }
 
     attachTradeLink(link)
   }
 
+  useEffect(() => {
+    setLink(user.steam_trade_url)
+  }, []);
+
   return (
     <div className={styles.trade}>
       <div className={styles.title}>
-        <img src={steam} alt="" />
-        Trade Link
+        <div className={styles.titleInfo}>
+          <img src={steam} alt="" />
+          Trade Link
+        </div>
+        {
+          !editTrade &&
+          <div className={styles.editBtn} onClick={handleEditTrade}>
+            <img src={edit} alt="" />
+          </div>
+        }
       </div>
 
       {
         user?.steam_trade_url ?
           <div className={styles.success}>
-            <div className={styles.attached}>
-              <img src={check} alt="" />
-              Ваша ссылка на обмен из Steam прикреплена
-            </div>
-            <div className={styles.linkWrapper}>
-              <div>
-                Ссылка
+            {
+              editTrade ?
+                <div className={styles.txt}>
+                  <img src={warning} alt="" />
+                  Прикрепите вашу ссылку на обмен из Steam
+                </div> :
+                <div className={styles.attached}>
+                  <img src={check} alt="" />
+                  Ваша ссылка на обмен из Steam прикреплена
+                </div>
+            }
+            {
+              editTrade ?
+                <div className={styles.input}>
+                  <div className={styles.subtitle}>
+                    Ссылка
+                  </div>
+                  <div className={`${styles.inputWrapper} ${isValid ? '' : styles.error}`}>
+                    <input type="text"
+                      disabled={!editTrade}
+                      placeholder='http//:'
+                      value={link}
+                      onChange={handleChange} />
+                  </div>
+                </div> :
+                <div className={styles.linkWrapper}>
+                  <div>
+                    Ссылка
+                  </div>
+                  <div className={styles.link}>
+                    {user.steam_trade_url}
+                  </div>
+                </div>
+            }
+            {
+              editTrade &&
+              <div className={styles.btns}>
+                <div onClick={Enter}>
+                  <Button title='Прикрепить' />
+                </div>
+                <div onClick={cancelEdit}>
+                  <Button color='brown'
+                    title='Отмена' />
+                </div>
               </div>
-              <div className={styles.link}>
-                {user.steam_trade_url}
-              </div>
-            </div>
+            }
           </div> :
           <div>
             <div className={styles.txt}>
@@ -97,20 +168,28 @@ const TradeLink = ({ handleTradeClick }) => {
               </div>
               <div className={`${styles.inputWrapper} ${isValid ? '' : styles.error}`}>
                 <input type="text"
+                  disabled={!editTrade}
                   placeholder='http//:'
                   value={link}
                   onChange={handleChange} />
               </div>
             </div>
-            <div className={styles.btns}>
-              <div onClick={Enter}>
-                <Button title='Прикрепить' />
-              </div>
-              <div onClick={handleTradeClick}>
-                <Button color='brown'
-                  title='Как получить ссылку?' />
-              </div>
-            </div>
+            {
+              editTrade ?
+                <div className={styles.btns}>
+                  <div onClick={Enter}>
+                    <Button title='Прикрепить' />
+                  </div>
+                  <div onClick={cancelEdit}>
+                    <Button color='brown'
+                      title='Отмена' />
+                  </div>
+                </div> :
+                <div className={styles.instruction}
+                  onClick={handleTradeClick}>
+                  Как получить ссылку
+                </div>
+            }
           </div>
       }
     </div>
