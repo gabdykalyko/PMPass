4dec5db96f710f56bcc04c77a4dc307eb70ee9f0
oral.gabdykalyk <oral.gabdykalyk@pmbet.kz>
2024-05-01 13:16:09 +0500

    feat: added Robot font

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,21 @@
 import styles from './Cards.module.scss'
 import quests from '../../assets/images/cards/quests.png'
 import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 const Cards = () => {
+  const { t } = useTranslation('main')
+
   return (
     <div className={`${styles.container} container-main ${styles.links}`}>
       <div className={styles.wrapper}>
         <NavLink to='/quests' 
                  className={`${styles.card} ${styles.quests}`}>
-          Квесты
+          {t('quests')}
         </NavLink>
         <NavLink to='/shop'
                  className={`${styles.card} ${styles.shop}`}>
-          Магазин
+          {t('shop')}
         </NavLink>
         <NavLink to='/profile'
                  className={`${styles.card} ${styles.profile}`}>
